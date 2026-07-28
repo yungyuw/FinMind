9b99274b1db2ca67dc7566730ccddaaa4b953dcf
Yungyu Wang <Yungyu_Wang@txone.com>
2026-07-28 13:32:41 +0800

    chore: support Python 3.14

--- genenv.py
+++ genenv.py
@@ -8,6 +8,9 @@ def get_config_from_environment(env_content):
     env_content += "FINMIND_PASSWORD={}\n".format(
         os.environ.get("FINMIND_PASSWORD", "")
     )
+    env_content += "FINMIND_API_TOKEN={}\n".format(
+        os.environ.get("FINMIND_API_TOKEN", "")
+    )
     return env_content
 
 
